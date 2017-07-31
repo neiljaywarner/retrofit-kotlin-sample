f3a4528be35338f613114fcc0ba3c0ea57775313
neilwarner <Neil.Warner@bswhealth.org>
2017-07-31 18:59:58 -0500

    Simplify code

--- app/src/main/java/com/segunfamisa/kotlin/samples/retrofit/MainActivity.kt
+++ app/src/main/java/com/segunfamisa/kotlin/samples/retrofit/MainActivity.kt
@@ -4,7 +4,6 @@ import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
 import android.util.Log
 import android.widget.TextView
-import com.segunfamisa.kotlin.samples.retrofit.data.kotlin.SearchRepository
 import com.segunfamisa.kotlin.samples.retrofit.data.kotlin.SearchRepositoryProvider
 
 import kotlinx.coroutines.experimental.android.UI
@@ -19,15 +18,14 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        // Note: the only code changes from this is to use textview
-        val repository = SearchRepositoryProvider.provideSearchRepository()
 
-        loadInfo(repository)
+        loadInfo()
 
     }
 
-    private fun loadInfo(repository: SearchRepository) = async(UI) {
+    private fun loadInfo() = async(UI) {
 
+        val repository = SearchRepositoryProvider.provideSearchRepository()
         val result = bg { repository.searchUsers("Lagos", "Java")}
         val size = result.await()?.size
         Log.d("Result", "There are ${size} Java developers in Lagos")
