b9d643377cac498ac1e92c80504df7ebf140f4c9
neilwarner <Neil.Warner@bswhealth.org>
2017-07-31 18:15:43 -0500

    Utilize textview so it more obviously works

--- app/src/main/java/com/segunfamisa/kotlin/samples/retrofit/MainActivity.kt
+++ app/src/main/java/com/segunfamisa/kotlin/samples/retrofit/MainActivity.kt
@@ -3,6 +3,8 @@ package com.segunfamisa.kotlin.samples.retrofit
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
 import android.util.Log
+import android.view.View
+import android.widget.TextView
 import com.segunfamisa.kotlin.samples.retrofit.data.kotlin.SearchRepositoryProvider
 import io.reactivex.android.schedulers.AndroidSchedulers
 import io.reactivex.disposables.CompositeDisposable
@@ -11,13 +13,13 @@ import io.reactivex.schedulers.Schedulers
 class MainActivity : AppCompatActivity() {
 
     val compositeDisposable: CompositeDisposable = CompositeDisposable()
+    val message : TextView by lazy { findViewById(R.id.message) as TextView }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
+        // Note: the only code changes from this is to use textview
         val repository = SearchRepositoryProvider.provideSearchRepository()
-
         compositeDisposable.add(
                 repository.searchUsers("Lagos", "Java")
                         .observeOn(AndroidSchedulers.mainThread())
@@ -25,6 +27,7 @@ class MainActivity : AppCompatActivity() {
                         .subscribe ({
                             result ->
                             Log.d("Result", "There are ${result.items.size} Java developers in Lagos")
+                            message.text = "There are ${result.items.size} Java developers in Lagos"
                         }, { error ->
                             error.printStackTrace()
                         })
